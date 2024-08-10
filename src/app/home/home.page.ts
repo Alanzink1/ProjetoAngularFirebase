254e07cac478e0deaf679e4a82fc052018628f2c
Alan Rodrigues <112907965+Alanzink1@users.noreply.github.com>
2024-08-10 00:29:22 +0000

    Montagem do Curriculo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,29 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
+  pessoa = {
+    foto: 'https://images-cdn.9gag.com/photo/agGK5vn_700b.jpg',
+    nome: 'Carl Johnson (CJ do GTA SA)',
+    funcao: 'Traficante, Gangster, Programador HTML & CSS',
+    contato: {
+      email: 'cjbrowherewegoagain@gmail.com',
+      telefone: '(15) 99100-4745',
+      github: '<a href="https://github.com/Alanzink1" target="_blank">/CjBro</a>',
+      linkedin: '<a href="https://br.linkedin.com/in/alanzink1" target="_blank">/CjDoSA</a>'
+    },
+    softskills: [
+      'Comunicação com os da gang',
+      'Proeficiência nas missões'
+    ],
+    formacao_academica: [
+      {
+        ano_inicio: '2023',
+        ano_termino: '2024',
+        instituicao: 'ETEC Sales Gomes',
+        curso: 'Desenvolvimento de Sistemas'
+      }
+    ]
+  }
+
   constructor(){ }
 }
