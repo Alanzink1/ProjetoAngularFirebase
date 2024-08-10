027bbd1f75ae155c34fac8c6f5a972c4140822f9
Alan Rodrigues <112907965+Alanzink1@users.noreply.github.com>
2024-08-10 01:49:37 +0000

    Primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,29 +11,42 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
-  pessoa = {
-    foto: 'https://images-cdn.9gag.com/photo/agGK5vn_700b.jpg',
-    nome: 'Carl Johnson (CJ do GTA SA)',
-    funcao: 'Traficante, Gangster, Programador HTML & CSS',
-    contato: {
-      email: 'cjbrowherewegoagain@gmail.com',
-      telefone: '(15) 99100-4745',
-      github: '<a href="https://github.com/Alanzink1" target="_blank">/CjBro</a>',
-      linkedin: '<a href="https://br.linkedin.com/in/alanzink1" target="_blank">/CjDoSA</a>'
-    },
-    softskills: [
-      'Comunicação com os da gang',
-      'Proeficiência nas missões'
-    ],
-    formacao_academica: [
-      {
-        ano_inicio: '2023',
-        ano_termino: '2024',
-        instituicao: 'ETEC Sales Gomes',
-        curso: 'Desenvolvimento de Sistemas'
-      }
-    ]
+  perfil = {
+    foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlVHV8GzIkxqHiNZK3D2SC5CUoU2qBunWd2A&s",
+    nome: "Carl Johnson (CJ)",
+    profissao: "Ator/Programador",
+    perfil_id: "@cj_do_gta",
+    idiomas: "Inglês, Arabe",
+    cidade: "Nova York",
+    desde: "desde Janeiro 1998",
+    biografia: "Oh shit, where we go again?",
+    curtidas: "1.5KK",
+    seguindo: "2",
+    amigos: "7"  
   }
 
-  constructor(){ }
+  postagens = {
+    amigo1: {
+    foto: "https://pbs.twimg.com/profile_images/1741493705884749825/tFPuUQW-_400x400.jpg",
+    nome: "Mipa zuzuzu",
+    nome_usuario: "@mipazu23",
+    post: "Hoje achei uma pedra no meu caminho, peguei e joguei ela no rio, até hoje não encontraram o corpo dele..."
+  },
+
+  amigo2: {
+    foto: "https://static.quizur.com/i/b/595c2b515de037.823180302017%20-%201%20(2).jpg",
+    nome: "Arelson Joelson",
+    nome_usuario: "@arejoel2012",
+    post: "Gosto de bolo, alguém sabe quem faz bolo?"
+  },
+
+  amigo3: {
+    foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXV9yWegptwQtQ-1gyUa10jXYh6Cr26HUYAUSIE4Nsxbn8pFw0DqKaoKKdCJRSBC-CBWQ&usqp=CAU",
+    nome: "Gerson Trevisan",
+    nome_usuario: "@gertrev22",
+    post: "Boa noite amigos, hoje vim lhes fazer uma piada... Piu! Até a noite que vem!"
+  }
+}
+
+
 }
