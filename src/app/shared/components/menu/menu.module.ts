71bf1a1ffc8cb09190b93ce820087e5635bf750a
Alan Rodrigues <112907965+Alanzink1@users.noreply.github.com>
2024-11-08 23:42:40 +0000

    tese

---
+++ src/app/shared/components/menu/menu.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu.component';
+import { RouterLink } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterLink,
+    IonicModule,
+    MenuComponent
+  ],
+  exports: [MenuComponent]
+})
+export class MenuModule { }
